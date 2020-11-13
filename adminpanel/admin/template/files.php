fe3dccbfff0092d5cdddd2a6752311afe544452d
dimaarion <dimaarion@gmail.com>
2020-11-13 09:05:40 +0200

    dimaarion/fk.git

--- adminpanel/admin/template/files.php
+++ adminpanel/admin/template/files.php
@@ -1,8 +1,26 @@
-<div id="images" class="col text-center"></div>
-<?php $controller->includer(true, true, './admin/template/headtitle.php', $controller, 'Файлы', ''); ?>
-<form id="filesForm" enctype="multipart/form-data" action="/adminpanel/files/load" method="post">
-  <div class="row mt-3">
-    <div class="col-3">
+<div
+id="images"
+class="col text-center">
+</div>
+  <?php
+  $controller->includer(
+    true,
+    true,
+    './admin/template/headtitle.php',
+    $controller,
+    'Файлы',
+    ''
+  );
+  ?>
+<form
+id="filesForm"
+enctype="multipart/form-data"
+action="/adminpanel/files/load"
+method="post">
+  <div
+  class="row mt-3">
+    <div
+    class="col-3">
       <?php
 
       $controller->inputs(
@@ -58,30 +76,76 @@
           $img =   imgFilter($value);
       ?>
 
-          <div class="col-sm-2 row images">
-            <div class="col"><img width="100%" src="<?php echo $img; ?>" alt="<?php echo $img; ?>" title="<?php echo $img; ?>"></div>
-            <button class="text-right deleteF" name="idfiles" value="../img/upload/<?php echo $value; ?>">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
-              </svg>
-            </button>
+          <div
+          class="col-sm-2 row images">
+            <div
+            class="col">
+            <div
+            class="titleImg">
+              <?php echo $value; ?>
+            </div>
+              <img
+              width="100%"
+              src="<?php echo $img; ?>"
+              alt="<?php echo $img; ?>">
+            </div>
+            <div class="buttonImg">
+              <button
+              class="text-right deleteF"
+              name="idfiles"
+              value="../img/upload/<?php echo $value; ?>">
+                <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                class="bi bi-trash-fill"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg">
+                  <path
+                  fill-rule="evenodd"
+                  d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
+                </svg>
+              </button>
+            </div>
+
           </div>
-        <?php  endif; if($controller->page != 'files') :
+        <?php
+      endif;
+      if($controller->page != 'files') :
           $img =   imgFilter($value);
         ?>
-  
-          <div class="col-sm-2 row images">
-            <div class="col"><img width="100%" src="<?php echo $img; ?>" alt="<?php echo $img; ?>" title="<?php echo $img; ?>"></div>
-            <button class="text-right deleteF" name="idfiles" value="../img/upload/<?php echo $value; ?>">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
+
+          <div
+          class="col-sm-2 row images">
+            <div
+            class="col">
+            <img
+            width="100%"
+            src="<?php echo $img; ?>"
+            alt="<?php echo $img; ?>"
+            title="<?php echo $img; ?>">
+          </div>
+            <button
+            class="text-right deleteF"
+            name="idfiles"
+            value="../img/upload/<?php echo $value; ?>">
+              <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-trash-fill"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg">
+                <path
+                fill-rule="evenodd"
+                d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" />
               </svg>
             </button>
           </div>
-
       <?php
         endif;
-      endforeach; ?>
+      endforeach;
+      ?>
     </div>
   </div>
 </form>
